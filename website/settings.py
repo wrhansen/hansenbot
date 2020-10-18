ac2a77c0494a3fbb347dcf919f7886336f5810af
Wesley Hansen <wesrhansen@gmail.com>
2020-10-18 10:22:09 -0400

    remove more cruft from old site

--- website/settings.py
+++ website/settings.py
@@ -26,9 +26,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "(c6q&p5zalqy_^5^e&l7yg$!qde2q6(@&eropgtawg!hjim%1+"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", True))
 
