bca1bcf12e5109cd0097b42d31067e8bdfa6bd62
Wesley Hansen <wesrhansen@gmail.com>
2020-12-20 01:41:24 -0500

    missing logging somehow

--- website/settings.py
+++ website/settings.py
@@ -33,6 +33,20 @@ ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(",")
 
 SECURE_SSL_REDIRECT = False
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
+
 
 # Application definition
 
