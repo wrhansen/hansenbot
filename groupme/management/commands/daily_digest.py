d8c9ee80523475a05d32a8ad0272fbaa699aad35
Wesley Hansen <wesrhansen@gmail.com>
2022-12-25 23:25:51 -0500

    ok, this should work

--- groupme/management/commands/daily_digest.py
+++ groupme/management/commands/daily_digest.py
@@ -6,8 +6,5 @@ from groupme.tasks import morning_digest
 class Command(BaseCommand):
     help = 'Runs Daily Digest'
 
-    def add_arguments(self, parser):
-        parser.add_argument('poll_ids', nargs='+', type=int)
-
     def handle(self, *args, **options):
         morning_digest()
